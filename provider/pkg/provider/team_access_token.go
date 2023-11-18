c4f52498ae5ab12800992ae3d1906a20176297b3
Komal <komal@pulumi.com>
2023-11-17 22:18:32 -0800

    Revert diff function changes

--- provider/pkg/provider/team_access_token.go
+++ provider/pkg/provider/team_access_token.go
@@ -60,7 +60,7 @@ func (t *PulumiServiceTeamAccessTokenResource) Name() string {
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	return considerAllChangesReplaces(req)
+	return diffImplForTokens(req)
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -173,4 +173,33 @@ func splitTeamAccessTokenId(id string) (string, string, string, string, error) {
 		return "", "", "", "", fmt.Errorf("%q is invalid, must contain a single slash ('/')", id)
 	}
 	return s[0], s[1], s[2], s[3], nil
+}
+
+func diffImplForTokens(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
+	olds, err := plugin.UnmarshalProperties(req.GetOlds(), plugin.MarshalOptions{KeepUnknowns: false, SkipNulls: true})
+	if err != nil {
+		return nil, err
+	}
+
+	news, err := plugin.UnmarshalProperties(req.GetNews(), plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: false})
+	if err != nil {
+		return nil, err
+	}
+
+	diffs := olds["__inputs"].ObjectValue().Diff(news)
+	if diffs == nil {
+		return &pulumirpc.DiffResponse{
+			Changes: pulumirpc.DiffResponse_DIFF_NONE,
+		}, nil
+	}
+
+	changes := pulumirpc.DiffResponse_DIFF_NONE
+	if diffs.Changed("description") {
+		changes = pulumirpc.DiffResponse_DIFF_SOME
+	}
+
+	return &pulumirpc.DiffResponse{
+		Changes:  changes,
+		Replaces: []string{"description"},
+	}, nil
 }
