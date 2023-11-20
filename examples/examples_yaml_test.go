pulumi/pulumi-pulumiservice

commit 6c88d5c141d6e653fadd72c8f16fa7f025bcea13

Allow refreshing org and team tokens (#191)
Fixes #159

provider/pkg/provider/access_token.go
@@ -40,32 +40,7 @@ func (at *PulumiServiceAccessTokenResource) Name() string {
 }
 
 func (at *PulumiServiceAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	olds, err := plugin.UnmarshalProperties(req.GetOlds(), plugin.MarshalOptions{KeepUnknowns: false, SkipNulls: true})
-	if err != nil {
-		return nil, err
-	}
-
-	news, err := plugin.UnmarshalProperties(req.GetNews(), plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: false})
-	if err != nil {
-		return nil, err
-	}
-
-	diffs := olds["__inputs"].ObjectValue().Diff(news)
-	if diffs == nil {
-		return &pulumirpc.DiffResponse{
-			Changes: pulumirpc.DiffResponse_DIFF_NONE,
-		}, nil
-	}
-
-	changes := pulumirpc.DiffResponse_DIFF_NONE
-	if diffs.Changed("description") {
-		changes = pulumirpc.DiffResponse_DIFF_SOME
-	}
-
-	return &pulumirpc.DiffResponse{
-		Changes:  changes,
-		Replaces: []string{"description"},
-	}, nil
+	return considerAllChangesReplaces(req)
 }
 
 func (at *PulumiServiceAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -114,19 +89,20 @@ func (at *PulumiServiceAccessTokenResource) Check(req *pulumirpc.CheckRequest) (
 	return &pulumirpc.CheckResponse{Inputs: req.News, Failures: nil}, nil
 }
 
-func (at *PulumiServiceAccessTokenResource) Update(req *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
-	return &pulumirpc.UpdateResponse{}, nil
+func (at *PulumiServiceAccessTokenResource) Update(_ *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
+	// all updates are destructive, so we just call Create.
+	return nil, fmt.Errorf("unexpected call to update, expected create to be called instead")
 }
 
 func (at *PulumiServiceAccessTokenResource) Read(req *pulumirpc.ReadRequest) (*pulumirpc.ReadResponse, error) {
 	ctx := context.Background()
 
 	// the access token is immutable; if we get nil it got deleted, otherwise all data is the same
-	accesstoken, err := at.getAccessToken(ctx, req.GetId())
+	accessToken, err := at.client.GetAccessToken(ctx, req.GetId())
 	if err != nil {
 		return nil, err
 	}
-	if accesstoken == nil {
+	if accessToken == nil {
 		return &pulumirpc.ReadResponse{}, nil
 	}
 
@@ -136,32 +112,23 @@ func (at *PulumiServiceAccessTokenResource) Read(req *pulumirpc.ReadRequest) (*p
 	}, nil
 }
 
-func (at *PulumiServiceAccessTokenResource) Invoke(s *pulumiserviceProvider, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+func (at *PulumiServiceAccessTokenResource) Invoke(_ *pulumiserviceProvider, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
 	return &pulumirpc.InvokeResponse{Return: nil}, fmt.Errorf("unknown function '%s'", req.Tok)
 }
 
-func (at *PulumiServiceAccessTokenResource) Configure(config PulumiServiceConfig) {
+func (at *PulumiServiceAccessTokenResource) Configure(_ PulumiServiceConfig) {
 }
 
 func (at *PulumiServiceAccessTokenResource) createAccessToken(ctx context.Context, input PulumiServiceAccessTokenInput) (*pulumiapi.AccessToken, error) {
 
-	accesstoken, err := at.client.CreateAccessToken(ctx, input.Description)
+	accessToken, err := at.client.CreateAccessToken(ctx, input.Description)
 	if err != nil {
 		return nil, err
 	}
 
-	return accesstoken, nil
+	return accessToken, nil
 }
 
 func (at *PulumiServiceAccessTokenResource) deleteAccessToken(ctx context.Context, tokenId string) error {
 	return at.client.DeleteAccessToken(ctx, tokenId)
-}
-
-func (at *PulumiServiceAccessTokenResource) getAccessToken(ctx context.Context, id string) (*pulumiapi.AccessToken, error) {
-	accesstoken, err := at.client.GetAccessToken(ctx, id)
-	if err != nil {
-		return nil, err
-	}
-
-	return accesstoken, nil
 }

provider/pkg/provider/org_access_token.go
@@ -59,35 +59,7 @@ func (ot *PulumiServiceOrgAccessTokenResource) Name() string {
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	olds, err := plugin.UnmarshalProperties(req.GetOlds(), plugin.MarshalOptions{KeepUnknowns: false, SkipNulls: true})
-	if err != nil {
-		return nil, err
-	}
-
-	news, err := plugin.UnmarshalProperties(req.GetNews(), plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: false})
-	if err != nil {
-		return nil, err
-	}
-
-	diffs := olds["__inputs"].ObjectValue().Diff(news)
-	if diffs == nil {
-		return &pulumirpc.DiffResponse{
-			Changes: pulumirpc.DiffResponse_DIFF_NONE,
-		}, nil
-	}
-
-	changes, replaces := pulumirpc.DiffResponse_DIFF_NONE, []string(nil)
-	if diffs.Changed("description") {
-		changes, replaces = pulumirpc.DiffResponse_DIFF_SOME, append(replaces, "description")
-	}
-	if diffs.Changed("admin") {
-		changes, replaces = pulumirpc.DiffResponse_DIFF_SOME, append(replaces, "admin")
-	}
-
-	return &pulumirpc.DiffResponse{
-		Changes:  changes,
-		Replaces: replaces,
-	}, nil
+	return considerAllChangesReplaces(req)
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -144,29 +116,47 @@ func (ot *PulumiServiceOrgAccessTokenResource) Check(req *pulumirpc.CheckRequest
 	return &pulumirpc.CheckResponse{Inputs: req.News, Failures: nil}, nil
 }
 
-func (ot *PulumiServiceOrgAccessTokenResource) Update(req *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
-	return &pulumirpc.UpdateResponse{}, nil
+func (ot *PulumiServiceOrgAccessTokenResource) Update(_ *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
+	// all updates are destructive, so we just call Create.
+	return nil, fmt.Errorf("unexpected call to update, expected create to be called instead")
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) Read(req *pulumirpc.ReadRequest) (*pulumirpc.ReadResponse, error) {
-	return &pulumirpc.ReadResponse{}, nil
+	ctx := context.Background()
+	urn := req.GetId()
+
+	orgName, _, tokenId, err := splitOrgAccessTokenId(urn)
+
+	// the org access token is immutable; if we get nil it got deleted, otherwise all data is the same
+	accessToken, err := ot.client.GetOrgAccessToken(ctx, tokenId, orgName)
+	if err != nil {
+		return nil, err
+	}
+	if accessToken == nil {
+		return &pulumirpc.ReadResponse{}, nil
+	}
+
+	return &pulumirpc.ReadResponse{
+		Id:         req.GetId(),
+		Properties: req.GetProperties(),
+	}, nil
 }
 
-func (ot *PulumiServiceOrgAccessTokenResource) Invoke(s *pulumiserviceProvider, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+func (ot *PulumiServiceOrgAccessTokenResource) Invoke(_ *pulumiserviceProvider, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
 	return &pulumirpc.InvokeResponse{Return: nil}, fmt.Errorf("unknown function '%s'", req.Tok)
 }
 
-func (ot *PulumiServiceOrgAccessTokenResource) Configure(config PulumiServiceConfig) {
+func (ot *PulumiServiceOrgAccessTokenResource) Configure(_ PulumiServiceConfig) {
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) createOrgAccessToken(ctx context.Context, input PulumiServiceOrgAccessTokenInput) (*pulumiapi.AccessToken, error) {
 
-	accesstoken, err := ot.client.CreateOrgAccessToken(ctx, input.Name, input.OrgName, input.Description, input.Admin)
+	accessToken, err := ot.client.CreateOrgAccessToken(ctx, input.Name, input.OrgName, input.Description, input.Admin)
 	if err != nil {
 		return nil, err
 	}
 
-	return accesstoken, nil
+	return accessToken, nil
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) deleteOrgAccessToken(ctx context.Context, id string) error {

provider/pkg/provider/team_access_token.go
@@ -59,32 +59,7 @@ func (t *PulumiServiceTeamAccessTokenResource) Name() string {
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	olds, err := plugin.UnmarshalProperties(req.GetOlds(), plugin.MarshalOptions{KeepUnknowns: false, SkipNulls: true})
-	if err != nil {
-		return nil, err
-	}
-
-	news, err := plugin.UnmarshalProperties(req.GetNews(), plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: false})
-	if err != nil {
-		return nil, err
-	}
-
-	diffs := olds["__inputs"].ObjectValue().Diff(news)
-	if diffs == nil {
-		return &pulumirpc.DiffResponse{
-			Changes: pulumirpc.DiffResponse_DIFF_NONE,
-		}, nil
-	}
-
-	changes := pulumirpc.DiffResponse_DIFF_NONE
-	if diffs.Changed("description") {
-		changes = pulumirpc.DiffResponse_DIFF_SOME
-	}
-
-	return &pulumirpc.DiffResponse{
-		Changes:  changes,
-		Replaces: []string{"description"},
-	}, nil
+	return considerAllChangesReplaces(req)
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -137,29 +112,47 @@ func (t *PulumiServiceTeamAccessTokenResource) Check(req *pulumirpc.CheckRequest
 	return &pulumirpc.CheckResponse{Inputs: req.News, Failures: nil}, nil
 }
 
-func (t *PulumiServiceTeamAccessTokenResource) Update(req *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
-	return &pulumirpc.UpdateResponse{}, nil
+func (t *PulumiServiceTeamAccessTokenResource) Update(_ *pulumirpc.UpdateRequest) (*pulumirpc.UpdateResponse, error) {
+	// all updates are destructive, so we just call Create.
+	return nil, fmt.Errorf("unexpected call to update, expected create to be called instead")
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Read(req *pulumirpc.ReadRequest) (*pulumirpc.ReadResponse, error) {
-	return &pulumirpc.ReadResponse{}, nil
+	ctx := context.Background()
+	urn := req.GetId()
+
+	orgName, teamName, _, tokenId, err := splitTeamAccessTokenId(urn)
+
+	// the team access token is immutable; if we get nil it got deleted, otherwise all data is the same
+	accessToken, err := t.client.GetTeamAccessToken(ctx, tokenId, orgName, teamName)
+	if err != nil {
+		return nil, err
+	}
+	if accessToken == nil {
+		return &pulumirpc.ReadResponse{}, nil
+	}
+
+	return &pulumirpc.ReadResponse{
+		Id:         req.GetId(),
+		Properties: req.GetProperties(),
+	}, nil
 }
 
-func (t *PulumiServiceTeamAccessTokenResource) Invoke(s *pulumiserviceProvider, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
+func (t *PulumiServiceTeamAccessTokenResource) Invoke(_ *pulumiserviceProvider, req *pulumirpc.InvokeRequest) (*pulumirpc.InvokeResponse, error) {
 	return &pulumirpc.InvokeResponse{Return: nil}, fmt.Errorf("unknown function '%s'", req.Tok)
 }
 
-func (t *PulumiServiceTeamAccessTokenResource) Configure(config PulumiServiceConfig) {
+func (t *PulumiServiceTeamAccessTokenResource) Configure(_ PulumiServiceConfig) {
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) createTeamAccessToken(ctx context.Context, input PulumiServiceTeamAccessTokenInput) (*pulumiapi.AccessToken, error) {
 
-	accesstoken, err := t.client.CreateTeamAccessToken(ctx, input.Name, input.OrgName, input.TeamName, input.Description)
+	accessToken, err := t.client.CreateTeamAccessToken(ctx, input.Name, input.OrgName, input.TeamName, input.Description)
 	if err != nil {
 		return nil, err
 	}
 
-	return accesstoken, nil
+	return accessToken, nil
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) deleteTeamAccessToken(ctx context.Context, id string) error {

provider/pkg/internal/pulumiapi/orgtokens.go
@@ -21,12 +21,6 @@ import (
 	"path"
 )
 
-type OrgAccessToken struct {
-	ID          string `json:"id"`
-	TokenValue  string `json:"tokenValue"`
-	Description string `json:"description"`
-}
-
 type createOrgTokenResponse struct {
 	ID         string `json:"id"`
 	TokenValue string `json:"tokenValue"`
@@ -38,7 +32,7 @@ type createOrgTokenRequest struct {
 	Admin       bool   `json:"admin"`
 }
 
-func (c *Client) CreateOrgAccessToken(ctx context.Context, name string, orgName string, description string, admin bool) (*AccessToken, error) {
+func (c *Client) CreateOrgAccessToken(ctx context.Context, name, orgName, description string, admin bool) (*AccessToken, error) {
 
 	if len(orgName) == 0 {
 		return nil, errors.New("empty orgName")
@@ -72,7 +66,7 @@ func (c *Client) CreateOrgAccessToken(ctx context.Context, name string, orgName 
 
 }
 
-func (c *Client) DeleteOrgAccessToken(ctx context.Context, tokenId string, orgName string) error {
+func (c *Client) DeleteOrgAccessToken(ctx context.Context, tokenId, orgName string) error {
 	if len(tokenId) == 0 {
 		return errors.New("tokenid length must be greater than zero")
 	}
@@ -91,4 +85,28 @@ func (c *Client) DeleteOrgAccessToken(ctx context.Context, tokenId string, orgNa
 	}
 
 	return nil
+}
+
+func (c *Client) GetOrgAccessToken(ctx context.Context, tokenId, orgName string) (*AccessToken, error) {
+	apiPath := path.Join("orgs", orgName, "tokens")
+
+	var listRes listTokenResponse
+
+	_, err := c.do(ctx, http.MethodGet, apiPath, nil, &listRes)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to list org access tokens: %w", err)
+	}
+
+	for i := 0; i < len(listRes.Tokens); i++ {
+		token := listRes.Tokens[i]
+		if token.ID == tokenId {
+			return &AccessToken{
+				ID:          token.ID,
+				Description: token.Description,
+			}, nil
+		}
+	}
+
+	return nil, nil
 }

provider/pkg/internal/pulumiapi/orgtokens_test.go
@@ -1,14 +1,12 @@
 package pulumiapi
 
 import (
-	"context"
+	"fmt"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-var orgCtx = context.Background()
 
 func TestDeleteOrgAccessToken(t *testing.T) {
 	orgName := "anOrg"
@@ -118,6 +116,63 @@ func TestCreateOrgAccessToken(t *testing.T) {
 		assert.EqualError(t,
 			err,
 			`failed to create access token: 401 API error: unauthorized`,
+		)
+	})
+}
+
+func TestGetOrgAccessToken(t *testing.T) {
+	id := "uuid"
+	desc := "token description"
+	org := "anOrg"
+	lastUsed := 123
+	t.Run("Happy Path", func(t *testing.T) {
+		resp := listTokenResponse{
+			Tokens: []accessTokenResponse{
+				{
+					ID:          id,
+					Description: desc,
+					LastUsed:    lastUsed,
+				},
+				{
+					ID:          "other",
+					Description: desc,
+					LastUsed:    lastUsed,
+				},
+			},
+		}
+		c, cleanup := startTestServer(t, testServerConfig{
+			ExpectedReqMethod: http.MethodGet,
+			ExpectedReqBody:   nil,
+			ExpectedReqPath:   fmt.Sprintf("/api/orgs/%s/tokens", org),
+			ResponseCode:      200,
+			ResponseBody:      resp,
+		})
+		defer cleanup()
+		token, err := c.GetOrgAccessToken(ctx, id, org)
+		assert.NoError(t, err)
+		assert.Equal(t, &AccessToken{
+			ID:          id,
+			Description: desc,
+		}, token)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		c, cleanup := startTestServer(t, testServerConfig{
+			ExpectedReqMethod: http.MethodGet,
+			ExpectedReqPath:   fmt.Sprintf("/api/orgs/%s/tokens", org),
+			ExpectedReqBody:   nil,
+			ResponseCode:      401,
+			ResponseBody: errorResponse{
+				StatusCode: 401,
+				Message:    "unauthorized",
+			},
+		})
+		defer cleanup()
+		token, err := c.GetOrgAccessToken(ctx, id, org)
+		assert.Nil(t, token, "token should be nil")
+		assert.EqualError(t,
+			err,
+			`failed to list org access tokens: 401 API error: unauthorized`,
 		)
 	})
 }

provider/pkg/internal/pulumiapi/teamtokens.go
@@ -21,12 +21,6 @@ import (
 	"path"
 )
 
-type TeamAccessToken struct {
-	ID          string `json:"id"`
-	TokenValue  string `json:"tokenValue"`
-	Description string `json:"description"`
-}
-
 type createTeamTokenResponse struct {
 	ID         string `json:"id"`
 	TokenValue string `json:"tokenValue"`
@@ -37,7 +31,7 @@ type createTeamTokenRequest struct {
 	Description string `json:"description"`
 }
 
-func (c *Client) CreateTeamAccessToken(ctx context.Context, name string, orgName string, teamName string, description string) (*AccessToken, error) {
+func (c *Client) CreateTeamAccessToken(ctx context.Context, name, orgName, teamName, description string) (*AccessToken, error) {
 
 	if len(orgName) == 0 {
 		return nil, errors.New("empty orgName")
@@ -70,7 +64,7 @@ func (c *Client) CreateTeamAccessToken(ctx context.Context, name string, orgName
 
 }
 
-func (c *Client) DeleteTeamAccessToken(ctx context.Context, tokenId string, orgName string, teamName string) error {
+func (c *Client) DeleteTeamAccessToken(ctx context.Context, tokenId, orgName, teamName string) error {
 	if len(tokenId) == 0 {
 		return errors.New("tokenid length must be greater than zero")
 	}
@@ -91,4 +85,28 @@ func (c *Client) DeleteTeamAccessToken(ctx context.Context, tokenId string, orgN
 	}
 
 	return nil
+}
+
+func (c *Client) GetTeamAccessToken(ctx context.Context, tokenId, orgName, teamName string) (*AccessToken, error) {
+	apiPath := path.Join("orgs", orgName, "teams", teamName, "tokens")
+
+	var listRes listTokenResponse
+
+	_, err := c.do(ctx, http.MethodGet, apiPath, nil, &listRes)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to list team access tokens: %w", err)
+	}
+
+	for i := 0; i < len(listRes.Tokens); i++ {
+		token := listRes.Tokens[i]
+		if token.ID == tokenId {
+			return &AccessToken{
+				ID:          token.ID,
+				Description: token.Description,
+			}, nil
+		}
+	}
+
+	return nil, nil
 }

provider/pkg/internal/pulumiapi/teamtokens_test.go
@@ -2,6 +2,7 @@ package pulumiapi
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -93,6 +94,64 @@ func TestCreateTeamAccessToken(t *testing.T) {
 		assert.EqualError(t,
 			err,
 			`failed to create access token: 401 API error: unauthorized`,
+		)
+	})
+}
+
+func TestGetTeamAccessToken(t *testing.T) {
+	id := "uuid"
+	desc := "token description"
+	org := "anOrg"
+	team := "aTeam"
+	lastUsed := 123
+	t.Run("Happy Path", func(t *testing.T) {
+		resp := listTokenResponse{
+			Tokens: []accessTokenResponse{
+				{
+					ID:          id,
+					Description: desc,
+					LastUsed:    lastUsed,
+				},
+				{
+					ID:          "other",
+					Description: desc,
+					LastUsed:    lastUsed,
+				},
+			},
+		}
+		c, cleanup := startTestServer(t, testServerConfig{
+			ExpectedReqMethod: http.MethodGet,
+			ExpectedReqBody:   nil,
+			ExpectedReqPath:   fmt.Sprintf("/api/orgs/%s/teams/%s/tokens", org, team),
+			ResponseCode:      200,
+			ResponseBody:      resp,
+		})
+		defer cleanup()
+		token, err := c.GetTeamAccessToken(ctx, id, org, team)
+		assert.NoError(t, err)
+		assert.Equal(t, &AccessToken{
+			ID:          id,
+			Description: desc,
+		}, token)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		c, cleanup := startTestServer(t, testServerConfig{
+			ExpectedReqMethod: http.MethodGet,
+			ExpectedReqPath:   fmt.Sprintf("/api/orgs/%s/teams/%s/tokens", org, team),
+			ExpectedReqBody:   nil,
+			ResponseCode:      401,
+			ResponseBody: errorResponse{
+				StatusCode: 401,
+				Message:    "unauthorized",
+			},
+		})
+		defer cleanup()
+		token, err := c.GetTeamAccessToken(ctx, id, org, team)
+		assert.Nil(t, token, "token should be nil")
+		assert.EqualError(t,
+			err,
+			`failed to list team access tokens: 401 API error: unauthorized`,
 		)
 	})
 }

commit fe512f8d23e6e82084ee20bd27db7b5856020b73

Fix diff functions for access tokens (#192)
Fixes a regression introduced in #191, fixes the diff functions for team
tokens and org tokens to correctly replace on changes and adds tests.

Also fixed a bug in teams where an update was detected based on the
order of team members.

Fixes #159
Fixes #158

examples/yaml-org-token/Pulumi.yaml
@@ -1,6 +1,6 @@
 name: pulumi-service-teams-access-token-yaml
 runtime: yaml
-description: An example of using python to create a team
+description: An example of using yaml to create an org token
 resources:
   rand:
     type: random:RandomString
@@ -12,5 +12,5 @@ resources:
     type: pulumiservice:index:OrgAccessToken
     properties:
       name: test-${rand.result}
-      organizationName: lbrlabs
+      organizationName: service-provider-test-org
       description: "example org access token"

examples/yaml-team-token/Pulumi.yaml
@@ -1,6 +1,6 @@
 name: pulumi-service-teams-access-token-yaml
 runtime: yaml
-description: An example of using python to create a team
+description: An example of using yaml to create a team token
 resources:
   rand:
     type: random:RandomString
@@ -15,6 +15,9 @@ resources:
       organizationName: service-provider-test-org
       displayName: PulumiUP Team
       teamType: pulumi
+      members:
+        - pulumi-bot
+        - service-provider-example-user
   token:
     type: pulumiservice:index:TeamAccessToken
     properties:

examples/yaml-teams/Pulumi.yaml
@@ -1,6 +1,6 @@
 name: pulumi-service-teams-example-yaml
 runtime: yaml
-description: An example of using python to create a team
+description: An example of using yaml to create a team
 resources:
   rand:
     type: random:RandomString
@@ -17,4 +17,3 @@ resources:
       teamType: pulumi
       members:
         - pulumi-bot
-        - service-provider-example-user

provider/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/pulumi/pulumi/pkg/v3 v3.94.2
 	github.com/pulumi/pulumi/sdk/v3 v3.94.2
 	github.com/stretchr/testify v1.8.4
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
 	google.golang.org/grpc v1.59.0
 	google.golang.org/protobuf v1.31.0
 )
@@ -171,7 +172,6 @@ require (
 	gocloud.dev v0.27.0 // indirect
 	gocloud.dev/secrets/hashivault v0.27.0 // indirect
 	golang.org/x/crypto v0.15.0 // indirect
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/mod v0.13.0 // indirect
 	golang.org/x/net v0.18.0 // indirect
 	golang.org/x/oauth2 v0.11.0 // indirect

provider/pkg/provider/access_token.go
@@ -41,7 +41,7 @@ func (at *PulumiServiceAccessTokenResource) Name() string {
 }
 
 func (at *PulumiServiceAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	return considerAllChangesReplaces(req)
+	return diffAccessTokenProperties(req, []string{"description"})
 }
 
 func (at *PulumiServiceAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -132,4 +132,40 @@ func (at *PulumiServiceAccessTokenResource) createAccessToken(ctx context.Contex
 
 func (at *PulumiServiceAccessTokenResource) deleteAccessToken(ctx context.Context, tokenId string) error {
 	return at.client.DeleteAccessToken(ctx, tokenId)
+}
+
+func diffAccessTokenProperties(req *pulumirpc.DiffRequest, replaceProps []string) (*pulumirpc.DiffResponse, error) {
+	olds, err := plugin.UnmarshalProperties(req.GetOlds(), plugin.MarshalOptions{KeepUnknowns: false, SkipNulls: true})
+	if err != nil {
+		return nil, err
+	}
+
+	news, err := plugin.UnmarshalProperties(req.GetNews(), plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: false})
+	if err != nil {
+		return nil, err
+	}
+
+	inputs, ok := olds["__inputs"]
+	if !ok {
+		return nil, fmt.Errorf("missing __inputs property")
+	}
+	diffs := inputs.ObjectValue().Diff(news)
+	if diffs == nil {
+		return &pulumirpc.DiffResponse{
+			Changes: pulumirpc.DiffResponse_DIFF_NONE,
+		}, nil
+	}
+
+	changes, replaces := pulumirpc.DiffResponse_DIFF_NONE, []string(nil)
+	for _, k := range replaceProps {
+		if diffs.Changed(resource.PropertyKey(k)) {
+			changes = pulumirpc.DiffResponse_DIFF_SOME
+			replaces = append(replaces, k)
+		}
+	}
+
+	return &pulumirpc.DiffResponse{
+		Changes:  changes,
+		Replaces: replaces,
+	}, nil
 }

provider/pkg/provider/org_access_token.go
@@ -60,7 +60,7 @@ func (ot *PulumiServiceOrgAccessTokenResource) Name() string {
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	return considerAllChangesReplaces(req)
+	return diffAccessTokenProperties(req, []string{"name", "organizationName", "description", "admin"})
 }
 
 func (ot *PulumiServiceOrgAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -170,7 +170,6 @@ func (ot *PulumiServiceOrgAccessTokenResource) deleteOrgAccessToken(ctx context.
 
 }
 
-// FIXME: we can likely create a util that will work for all cases
 func splitOrgAccessTokenId(id string) (string, string, string, error) {
 	// format: organization/name/tokenId
 	s := strings.Split(id, "/")

provider/pkg/provider/team.go
@@ -7,6 +7,7 @@ import (
 	"reflect"
 	"strings"
 
+	"golang.org/x/exp/slices"
 	"google.golang.org/grpc/codes"
 	pbempty "google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/structpb"
@@ -82,6 +83,9 @@ func ToPulumiServiceTeamInput(inputMap resource.PropertyMap) PulumiServiceTeamIn
 				input.Members = append(input.Members, m.StringValue())
 			}
 		}
+
+		// Sort the members so the order is deterministic
+		slices.Sort(input.Members)
 	}
 
 	if inputMap["organizationName"].HasValue() && inputMap["organizationName"].IsString() {
@@ -203,6 +207,8 @@ func (t *PulumiServiceTeamResource) Read(req *pulumirpc.ReadRequest) (*pulumirpc
 	for _, m := range team.Members {
 		inputs.Members = append(inputs.Members, m.GithubLogin)
 	}
+	// Sort the members so the order is deterministic
+	slices.Sort(inputs.Members)
 	props, err := plugin.MarshalProperties(inputs.ToPropertyMap(), plugin.MarshalOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal inputs to properties: %w", err)
@@ -233,14 +239,17 @@ func (t *PulumiServiceTeamResource) Update(req *pulumirpc.UpdateRequest) (*pulum
 		inputsChanged.Description = teamNew.Description
 		inputsChanged.DisplayName = teamNew.DisplayName
 
-		t.updateTeam(context.Background(), inputsChanged)
+		err = t.updateTeam(context.Background(), inputsChanged)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	// github teams can't manage membership.
-	if !Equal(teamOld.Members, teamNew.Members) && teamNew.Type != "github" {
+	if !slices.Equal(teamOld.Members, teamNew.Members) && teamNew.Type != "github" {
 		inputsChanged.Members = teamNew.Members
 		for _, usernameToDelete := range teamOld.Members {
-			if !InSlice(usernameToDelete, teamNew.Members) {
+			if !slices.Contains(teamNew.Members, usernameToDelete) {
 				err := t.deleteFromTeam(ctx, teamNew.OrganizationName, teamNew.Name, usernameToDelete)
 				if err != nil {
 					return nil, err
@@ -249,7 +258,7 @@ func (t *PulumiServiceTeamResource) Update(req *pulumirpc.UpdateRequest) (*pulum
 		}
 
 		for _, usernameToAdd := range teamNew.Members {
-			if !InSlice(usernameToAdd, teamOld.Members) {
+			if !slices.Contains(teamOld.Members, usernameToAdd) {
 				err := t.addToTeam(ctx, teamNew.OrganizationName, teamNew.Name, usernameToAdd)
 				if err != nil {
 					return nil, err

provider/pkg/provider/team_access_token.go
@@ -60,7 +60,7 @@ func (t *PulumiServiceTeamAccessTokenResource) Name() string {
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Diff(req *pulumirpc.DiffRequest) (*pulumirpc.DiffResponse, error) {
-	return considerAllChangesReplaces(req)
+	return diffAccessTokenProperties(req, []string{"name", "organizationName", "teamName", "description"})
 }
 
 func (t *PulumiServiceTeamAccessTokenResource) Delete(req *pulumirpc.DeleteRequest) (*pbempty.Empty, error) {
@@ -165,7 +165,6 @@ func (t *PulumiServiceTeamAccessTokenResource) deleteTeamAccessToken(ctx context
 
 }
 
-// FIXME: we can likely create a util that will work for all cases
 func splitTeamAccessTokenId(id string) (string, string, string, string, error) {
 	// format: organization/teamName/tokenName/tokenId
 	s := strings.Split(id, "/")

provider/pkg/provider/utils.go
@@ -1,28 +1,0 @@
-package provider
-
-import "sort"
-
-func Equal(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	sort.Strings(a)
-	sort.Strings(b)
-
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func InSlice(a string, b []string) bool {
-	for _, v := range b {
-		if v == a {
-			return true
-		}
-	}
-	return false
-}

examples/examples_yaml_test.go
@@ -113,9 +113,8 @@ func TestYamlTeamsExample(t *testing.T) {
 	t.Run("Yaml Teams Example", func(t *testing.T) {
 		cwd, _ := os.Getwd()
 		integration.ProgramTest(t, &integration.ProgramTestOptions{
-			Quick:       true,
-			SkipRefresh: true,
-			Dir:         path.Join(cwd, ".", "yaml-teams"),
+			Quick: true,
+			Dir:   path.Join(cwd, ".", "yaml-teams"),
 			// don't prepare project at all, not required for yaml
 			PrepareProject: func(_ *engine.Projinfo) error {
 				return nil
@@ -191,6 +190,28 @@ func TestYamlDeploymentSettingsExample(t *testing.T) {
 				Dir:             tmpdir,
 				ExpectNoChanges: true,
 			},
+		},
+	})
+}
+
+func TestYamlTeamAccessTokenExample(t *testing.T) {
+	cwd, _ := os.Getwd()
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Quick: true,
+		Dir:   path.Join(cwd, ".", "yaml-team-token"),
+		PrepareProject: func(_ *engine.Projinfo) error {
+			return nil
+		},
+	})
+}
+
+func TestYamlOrgAccessTokenExample(t *testing.T) {
+	cwd, _ := os.Getwd()
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Quick: true,
+		Dir:   path.Join(cwd, ".", "yaml-org-token"),
+		PrepareProject: func(_ *engine.Projinfo) error {
+			return nil
 		},
 	})
 }