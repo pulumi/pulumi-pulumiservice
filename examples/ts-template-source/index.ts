01ccaf9f505e83c297af519cd34a3c907a44b5d9
Sean Yeh <109418+seanyeh@users.noreply.github.com>
2024-09-25 18:45:31 -0500

    Rename example template

--- examples/ts-template-source/index.ts
+++ examples/ts-template-source/index.ts
@@ -3,7 +3,7 @@ import * as service from "@pulumi/pulumiservice";
 
 const source = new service.TemplateSource("source", {
     organizationName: "service-provider-test-org",
-    sourceName: "bootstrap",
+    sourceName: "bootstrap-ts",
     sourceURL: "https://github.com/pulumi/pulumi-pulumiservice",
     destination: {
       url: "https://github.com/pulumi/pulumi-pulumiservice"
