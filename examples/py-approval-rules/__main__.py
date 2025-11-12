07d4f9f0cfa0930c5dd21d37232342de092f7b8f
Jonathan Davenport <jonathan.davenport@gmail.com>
2025-11-12 09:18:46 -0500

    fix: remove deactivated user references from integration tests (#596)
    removes deactivated user references from integration tests to fix failing tests

--- examples/py-approval-rules/__main__.py
+++ examples/py-approval-rules/__main__.py
@@ -39,9 +39,6 @@ approval_rule = ApprovalRule(
             EligibleApproverArgs(
                 user="pulumi-bot",
             ),
-            EligibleApproverArgs(
-                user="IaroslavTitov",
-            ),
         ]
     )
 )
