1a3fe8ec7ef1aa8c37cb558f7e674c2580d78474
Ian Wahbe <me@iwahbe.com>
2025-11-18 14:58:28 +0100

    Use an explicitly created stack

--- examples/ts-stack-tags/index.ts
+++ examples/ts-stack-tags/index.ts
@@ -1,5 +1,17 @@
 import * as pulumiservice from "@pulumi/pulumiservice";
 import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
+
+// Create the stack resource first so it exists before we reference it
+const stackSuffix = new random.RandomPet("stack-suffix", {
+  prefix: pulumi.getStack(),
+  separator: "-",
+});
+const stack = new pulumiservice.Stack("test-stack", {
+  organizationName: "service-provider-test-org",
+  projectName: pulumi.getProject(),
+  stackName: stackSuffix.id,
+});
 
 export const stackTags = {
   "test-tag": "test-value",
@@ -9,9 +21,9 @@ export const stackTags = {
 
 Object.entries(stackTags).forEach(([k, v]) => {
   new pulumiservice.StackTag(k, {
-    organization: "service-provider-test-org",
-    project: pulumi.getProject(),
-    stack: pulumi.getStack(),
+    organization: stack.organizationName,
+    project: stack.projectName,
+    stack: stack.stackName,
     name: k,
     value: v,
   });
--- examples/ts-team-stack-permissions/index.ts
+++ examples/ts-team-stack-permissions/index.ts
@@ -1,7 +1,19 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as service from "@pulumi/pulumiservice";
+import * as random from "@pulumi/random";
 
 let config = new pulumi.Config();
+
+// Create the stack resource first so it exists before we reference it
+const stackSuffix = new random.RandomPet("stack-suffix", {
+  prefix: pulumi.getStack(),
+  separator: "-",
+});
+const stack = new service.Stack("test-stack", {
+  organizationName: "service-provider-test-org",
+  projectName: pulumi.getProject(),
+  stackName: stackSuffix.id,
+});
 
 const team = new service.Team("team", {
   organizationName: "service-provider-test-org",
@@ -12,8 +24,8 @@ const team = new service.Team("team", {
 
 new service.TeamStackPermission("team-permission", {
   organization: team.organizationName,
-  project: pulumi.getProject(),
-  stack: pulumi.getStack(),
+  project: stack.projectName,
+  stack: stack.stackName,
   team: team.name as pulumi.Output<string>,
   permission: service.TeamStackPermissionScope.Admin,
 });
